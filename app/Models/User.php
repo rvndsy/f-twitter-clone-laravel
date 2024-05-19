80c55255f62a6f19f848980b056d5439ba2310a8
probablyarlight <112282114+probablyalright@users.noreply.github.com>
2024-05-19 18:29:31 +0000

    Added Provider AuthServiceProvider

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +10,7 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory,  Notifiable;
 
     /**
      * The attributes that are mass assignable.
