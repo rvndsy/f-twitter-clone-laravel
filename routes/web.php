667cdd62a877dfda24c63981acd362fb95a952a8
probablyalright <112282114+probablyalright@users.noreply.github.com>
2024-05-22 09:21:27 +0000

    Add login/registration by Breeze+Blade

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
-    // Hello
 });
 
-Route::group([
-    'as' => 'passport.',
-    'prefix' => config('passport.path', 'oauth'),
-    'namespace' => '\Laravel\Passport\Http\Controllers',
-], function () {
-    // Passport routes...
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('posts', PostController::class);
+require __DIR__.'/auth.php';
