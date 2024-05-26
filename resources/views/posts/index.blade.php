dc278ec4778ecfc73454f85e49574b9f2cef33bb
probablyalright <112282114+probablyalright@users.noreply.github.com>
2024-05-26 10:54:12 +0000

    Clean up posts/index.blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto">
         <form method="POST" action="{{ route('posts.store') }}">
-            @csrf
+            @csrf <!-- Cross-site request forgery protection -->
             <textarea name="message" placeholder="{{ __('Type your post here...') }}" class="block w-full border-gray-400 h-40">{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-1" />
             <x-primary-button class="mt-4">{{ __('Post') }}</x-primary-button>
@@ -10,12 +10,13 @@
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($posts as $post)
             <div class="p-6 flex space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" \ fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" 
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 
-                                                                                4.418-4.03 8-9 8a9.863 9.863 0 01-4.2
-                                                                                55-.949L3 20l1.395-3.72C3.512 15.042 
-                                                                                3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.
-                                                                                582 9 8z" />
+                                                                                    4.418-4.03 8-9 8a9.863 9.863 0 01-4.2
+                                                                                    55-.949L3 20l1.395-3.72C3.512 15.042 
+                                                                                    3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.
+                                                                                    582 9 8z" />
                 </svg>
 
                 <div class="flex-1">
@@ -28,9 +29,9 @@
                         <x-dropdown>
                             <x-slot name="trigger">
                                 <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 
-                                                10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400"
+                                    viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2
                                                  2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </button>
@@ -46,7 +47,7 @@
                             </x-slot>
                         </x-dropdown>
                         @endif
-                    </div <p class="mt-3 text-lg text-gray-900">{{ $post->message }}</p>
+                    </div class="mt-3 text-lg text-gray-900">{{ $post->message }}</p>
                 </div>
             </div>
             @endforeach
